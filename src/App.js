6afc92095d4a1e49a23585661267da965ea86a29
Neha Hunnur <hunnurneha@gmail.com>
2024-08-08 15:07:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching countries:", error));
+  }, []);
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search for countries..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className="country-grid">
+        {filteredCountries.map((country) => (
+          <div className="countryCard" key={country.cca3}>
+            <img
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+            />
+            <p>{country.name.common}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
